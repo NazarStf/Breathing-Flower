285e989459b6b68393d5a8f00b1c7e9304d9976c
NazarStf <hworker98@gmail.com>
2023-07-27 22:28:14 +0300

    Initial Commit

---
+++ Breathing Flower/Breathing_FlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breathing_FlowerApp.swift
+//  Breathing Flower
+//
+//  Created by NazarStf on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Breathing_FlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
